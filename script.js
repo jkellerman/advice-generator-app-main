aafba74b285f58a1e65872e837dc28c3d0dd5899
jkellerman <89940123+jkellerman@users.noreply.github.com>
2022-03-21 21:11:58 +0000

    fetch data from advice slip API

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
-console.log("hello");
+const num = document.querySelector(".advice__num");
+const quote = document.querySelector(".quote");
+const dice = document.querySelector(".dice__container");
+const url = "https://api.adviceslip.com/advice";
+
+function getData() {
+  fetch(url)
+    .then((r) => r.json())
+    .then((jsonResponse) => {
+      let data = jsonResponse.slip;
+      let adviceNum = data.id;
+      let advice = data.advice;
+
+      num.innerText = `Advice #${adviceNum}`;
+      quote.innerText = `"${advice}"`;
+    });
+}
+
+getData();
+
+dice.addEventListener("click", () => {
+  getData();
+});
